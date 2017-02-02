da68c3e8a2372e7386d6fea35afa4707b0789104
Jotham Apaloo <jothamapaloo@gmail.com>
2017-02-01 19:27:55 -0800

    Fixup types and remove unused functions

--- sisosig/sisosig.py
+++ sisosig/sisosig.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-
 import os
 import requests
 from datetime import datetime
 
-# def _get(lat: float, lon: float, :key str, template: str) -> dict:
-#     endpoint = template.format() #type: str
-#     return requests.get(endpoint).json()
 
 class DarkskyClient:
-    def __init__(self, key: str):
+    def __init__(self, key: str) -> None:
         self.key = key
 
     def get_forecast(self, lat: float, lon: float) -> dict:
@@ -32,6 +28,3 @@ class DarkskyClient:
                                    time=time.timestamp()) #type: str
 
         return requests.get(endpoint).json()
-
-def save_forecast(forecast: dict) -> None:
-    ...
